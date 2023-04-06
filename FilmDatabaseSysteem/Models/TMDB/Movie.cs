1302c8ebf5d9441ec0c239816ad4732b2488b8df
Bart <barth@laptop-ldbsqcdl>
2023-04-06 20:16:08 +0200

    - Properties in Movie.cs correct gelinkt aan de JSON zodat alle data er uit gehaald wordt. - Favorieten.cshtml is nu geen statische pagina met text meer maar heeft opmaak en wordt gegenereerd vanaf een lijst in Favorieten.cshtml.cs

--- FilmDatabaseSysteem/Models/TMDB/Movie.cs
+++ FilmDatabaseSysteem/Models/TMDB/Movie.cs
@@ -2,29 +2,32 @@
 
 public class Movie
 {
-    public bool Adult { get; set; }
-    public string BackdropPath { get; set; }
+    [JsonProperty("adult")] public bool Adult { get; set; }
+    [JsonProperty("backdrop_path")] public string BackdropPath { get; set; }
+
+    [JsonProperty("belongs_to_collection")]
     public object BelongsToCollection { get; set; }
-    public int Budget { get; set; }
-    public List<Genre> Genres { get; set; }
-    public string Homepage { get; set; }
-    public int Id { get; set; }
-    public string ImdbId { get; set; }
-    public string OriginalLanguage { get; set; }
-    public string OriginalTitle { get; set; }
-    public string Overview { get; set; }
-    public double Popularity { get; set; }
-    public string PosterPath { get; set; }
-    public List<ProductionCompany> ProductionCompanies { get; set; }
-    public List<ProductionCountry> ProductionCountries { get; set; }
-    public string ReleaseDate { get; set; }
-    public int Revenue { get; set; }
-    public int Runtime { get; set; }
-    public List<SpokenLanguage> SpokenLanguages { get; set; }
-    public string Status { get; set; }
-    public string Tagline { get; set; }
-    public string Title { get; set; }
-    public bool Video { get; set; }
-    public double VoteAverage { get; set; }
-    public int VoteCount { get; set; }
+
+    [JsonProperty("budget")] public int Budget { get; set; }
+    [JsonProperty("genres")] public List<Genre> Genres { get; set; }
+    [JsonProperty("homepage")] public string Homepage { get; set; }
+    [JsonProperty("id")] public int Id { get; set; }
+    [JsonProperty("imdb_id")] public string ImdbId { get; set; }
+    [JsonProperty("original_language")] public string OriginalLanguage { get; set; }
+    [JsonProperty("original_title")] public string OriginalTitle { get; set; }
+    [JsonProperty("overview")] public string Overview { get; set; }
+    [JsonProperty("popularity")] public double Popularity { get; set; }
+    [JsonProperty("poster_path")] public string PosterPath { get; set; }
+    [JsonProperty("production_companies")] public List<ProductionCompany> ProductionCompanies { get; set; }
+    [JsonProperty("production_countries")] public List<ProductionCountry> ProductionCountries { get; set; }
+    [JsonProperty("release_date")] public string ReleaseDate { get; set; }
+    [JsonProperty("revenue")] public int Revenue { get; set; }
+    [JsonProperty("runtime")] public int Runtime { get; set; }
+    [JsonProperty("spoken_languages")] public List<SpokenLanguage> SpokenLanguages { get; set; }
+    [JsonProperty("status")] public string Status { get; set; }
+    [JsonProperty("tagline")] public string Tagline { get; set; }
+    [JsonProperty("title")] public string Title { get; set; }
+    [JsonProperty("video")] public bool Video { get; set; }
+    [JsonProperty("vote_average")] public double VoteAverage { get; set; }
+    [JsonProperty("vote_count")] public int VoteCount { get; set; }
 }
