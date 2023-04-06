2dd993249eb14b5ff4fca35bab8440b47428861d
Bart <barth@laptop-ldbsqcdl>
2023-04-06 15:36:54 +0200

    TDMBService.cs class gemaakt die verbindt met de API. Movie.cs gemaakt waar de JSON informatie in kan. Aantal subclasses gemaakt tbhv Movie.cs Proof of Concept van de favorietenpagina gemaakt.

---
+++ FilmDatabaseSysteem/Models/Favorieten.cs
@@ -0,0 +1,16 @@
+﻿using FilmDatabaseSysteem.Pages;
+
+namespace FilmDatabaseSysteem.Models
+{
+    public class Favorieten
+    {
+        public int UserId { get; set; }
+        public List<int> MovieIds { get; set; }
+
+        public Favorieten(int userId, List<int> movieIds)
+        {
+            UserId = userId;
+            MovieIds = movieIds;
+        }
+    }
+}
---
+++ FilmDatabaseSysteem/Models/TMDB/Movie.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+public class Movie
+{
+    public bool Adult { get; set; }
+    public string BackdropPath { get; set; }
+    public object BelongsToCollection { get; set; }
+    public int Budget { get; set; }
+    public List<Genre> Genres { get; set; }
+    public string Homepage { get; set; }
+    public int Id { get; set; }
+    public string ImdbId { get; set; }
+    public string OriginalLanguage { get; set; }
+    public string OriginalTitle { get; set; }
+    public string Overview { get; set; }
+    public double Popularity { get; set; }
+    public string PosterPath { get; set; }
+    public List<ProductionCompany> ProductionCompanies { get; set; }
+    public List<ProductionCountry> ProductionCountries { get; set; }
+    public string ReleaseDate { get; set; }
+    public int Revenue { get; set; }
+    public int Runtime { get; set; }
+    public List<SpokenLanguage> SpokenLanguages { get; set; }
+    public string Status { get; set; }
+    public string Tagline { get; set; }
+    public string Title { get; set; }
+    public bool Video { get; set; }
+    public double VoteAverage { get; set; }
+    public int VoteCount { get; set; }
+}
