b697018767066468dd40177bfcc704e12060fb46
Emiel <emielbaur@outlook.com>
2023-04-12 10:46:30 +0200

    film pagina functionaliteit

--- FilmDatabaseSysteem/Pages/Film.cshtml.cs
+++ FilmDatabaseSysteem/Pages/Film.cshtml.cs
@@ -1,12 +1,30 @@
+using FilmDatabaseSysteem.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace FilmDatabaseSysteem.Pages
 {
     public class FilmModel : PageModel
     {
-        public void OnGet()
+        public Movie movies { get; set; }
+        public List<Movie> Movies { get; set; }
+
+        public IActionResult OnGet()
         {
+            TMDBService service = new TMDBService();
+
+            movies = new Movie();
+            Movies = new List<Movie>();
+
+            //foreach (var movieId in movies.MovieIds)
+            //{
+                var item = service.GetMovieDetails(550).Result;
+                Movie movie = JsonConvert.DeserializeObject<Movie>(item);
+             //   Movies.Add(movie);
+            //}
+
+            return Page();
         }
     }
 }
