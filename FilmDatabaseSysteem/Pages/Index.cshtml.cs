21389b192bdfe7d476d5c2c58adb094c3834b92b
jsimonis <js@appsforce.org>
2023-04-18 11:32:56 +0200

    auth update WIP

--- FilmDatabaseSysteem/Pages/Index.cshtml.cs
+++ FilmDatabaseSysteem/Pages/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace FilmDatabaseSysteem.Pages
@@ -15,6 +16,8 @@ namespace FilmDatabaseSysteem.Pages
     public class IndexModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
         public List<Movie> Movies { get; set; }
 
         [BindProperty(SupportsGet = true)]
@@ -26,12 +29,14 @@ namespace FilmDatabaseSysteem.Pages
             _signInManager = signInManager;
         }
 
-
+        [Authorize]
         public async Task<IActionResult> OnGetAsync()
         {
             TMDBService service = new TMDBService();
             var trendingMovies = await service.GetTrendingMovies();
             ViewData["TrendingMovies"] = trendingMovies;
+            //ViewData["UserName"] = "test"; 
+            //var user2 = await _userManager.GetUserAsync(User);
 
             if (!string.IsNullOrEmpty(SearchString))
             {
@@ -40,11 +45,16 @@ namespace FilmDatabaseSysteem.Pages
                 ViewData["TrendingMovies"] = movies;
             }
 
-            var user = await _signInManager.UserManager.GetUserAsync(User);
-            if (user != null)
-            {
-                ViewData["UserName"] = user.UserName;
-            }
+
+            //string userName = User.FindFirst(ClaimTypes.Name)?.Value;
+            //string userEmail = User.FindFirst(ClaimTypes.Email)?.Value;
+
+            //var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            //if (userId != null)
+            //{
+            //    var user = await _userManager.FindByIdAsync(userId);
+            //    ViewData["UserName"] = user.UserName;
+            //}
 
 
             return Page();
